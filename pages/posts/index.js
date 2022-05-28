76eff2707f75cacb1949011d4b28f373459c4e08
mstanka <stmmarketa@gmail.com>
2022-05-28 14:42:07 +0200

    add posts 2015, 2016, adjust latests posts styles

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,6 +3,7 @@ import matter from "gray-matter";
 import path from "path";
 import CustomLink from "../../components/CustomLink";
 import { postFilePaths, POSTS_PATH } from "../../utils/mdxUtils";
+import Image from 'next/image'
 
 export default function Posts({ posts }) {
   return (
@@ -10,14 +11,31 @@ export default function Posts({ posts }) {
       <CustomLink href="/" className="block mb-6">
         <a className="link">⬅️ Domů</a>
       </CustomLink>
+      <h1>Nejnovější příspěvky</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.filePath}>
+          <li key={post.filePath} className="py-2">
             <CustomLink
               as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`}
               href={`/posts/[slug]`}
             >
-              <a>{post.data.title}</a>
+              <a>
+                <div className="flex flex-col sm:flex-row items-center justify-between border border-stone-300 p-2 rounded-sm shadow-md">
+                    <Image
+                      src={post.data.coverImage}
+                      alt={post.data.title}
+                      width={230}
+                      height={140}
+                      priority
+                      className="next-image"
+                    />
+                    <div className="flex-1 p-6 text-center">
+                      <h2>{post.data.title.substring(11)}</h2>
+                      <h3>{post.data.title.substring(0,11)}</h3>
+                  </div>
+                  <div className="bg-cyan-700 text-stone-200 px-2 py-1 rounded-full ml-4"> {post.data.tags}</div>
+                </div>
+              </a>
             </CustomLink>
           </li>
         ))}
