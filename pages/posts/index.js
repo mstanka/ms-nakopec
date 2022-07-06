df17a3e370738fc0687ccf589c63eab214ec918e
mstanka <stmmarketa@gmail.com>
2022-07-06 12:09:46 +0200

    update style cardlist

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -14,27 +14,27 @@ export default function Posts({ posts }) {
       <h1>Nejnovější příspěvky</h1>
       <ul className="flex flex-wrap items-stretch justify-center gap-4">
         {posts.sort((a, b) => a.filePath - b.filePath).map((post) => (
-          <li key={post.filePath}>
-            <CustomLink
+          <li key={post.filePath} className="flex flex-col justify-between text-center border border-stone-300 px-4 py-6 rounded-sm shadow-md">
+            <div>
+              <CustomLink
               as={`/posts/${post.filePath.replace(/\.mdx?$/, "")}`}
               href={`/posts/[slug]`}
-            >
-              <div className="flex flex-col text-center justify-center items-center border border-stone-300 px-4 py-6 rounded-sm shadow-md">
-                  <Image
-                    src={post.data.coverImage}
-                    alt={post.data.title}
-                    width={230}
-                    height={140}
-                    priority
-                    className="next-image"
-                  />
-                  <div className="flex-1 py-6 text-center">
-                    <h2 className="w-52 m-auto">{post.data.title.substring(11)}</h2>
-                    <h3>{post.data.title.substring(0,11)}</h3>
-                </div>
-                <div className="bg-cyan-700 text-stone-200 px-4 py-1 rounded-full"> {post.data.tags}</div>
-              </div>
-            </CustomLink>
+              >
+                <Image
+                  src={post.data.coverImage}
+                  alt={post.data.title}
+                  width={230}
+                  height={140}
+                  priority
+                  className="next-image"
+                />
+              </CustomLink>
+            </div>
+            <div className="py-6 text-center">
+              <h2 className="w-52 m-auto">{post.data.title.substring(11)}</h2>
+              <h3>{post.data.title.substring(0,11)}</h3>
+            </div>
+            <div className="bg-cyan-700 text-stone-200 px-4 py-1 rounded-full -mt-1"> {post.data.tags}</div>
           </li>
         ))}
       </ul>
