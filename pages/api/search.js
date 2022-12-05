c00708f4443928891f75fe6e35afcf13f0b8bb7a
mstanka <stmmarketa@gmail.com>
2022-12-05 22:04:17 +0100

    query toLowerCase

--- pages/api/search.js
+++ pages/api/search.js
@@ -9,7 +9,7 @@ const posts =
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req, res) => {
   console.log(req.query.q)
-  const myQuery = req.query.q;
+  const myQuery = req.query.q.toLowerCase();
   const results = myQuery
     ? posts.filter((post) => post.data.title.toLowerCase().includes(myQuery))
     : [];
