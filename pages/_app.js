72f0bf918399b7f849135696e849172663b5b767
mstanka <stmmarketa@gmail.com>
2023-08-06 10:29:08 +0200

    add gtm

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { ThemeProvider } from "next-themes";
 import { MDXProvider } from "@mdx-js/react";
-import Script from "next/script";
 import Layout from "../components/Layout";
 import Footer from "../components/Footer";
 import CustomImage from "../components/CustomImage";
@@ -20,8 +19,6 @@ const components = {
 };
 
 export default function MyApp({ Component, pageProps }) {
-  const NEXT_PUBLIC_GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-
   return (
     <ThemeProvider attribute="class">
       <MDXProvider components={components}>
@@ -32,26 +29,6 @@ export default function MyApp({ Component, pageProps }) {
             content="initial-scale=1.0, width=device-width"
           />
         </Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          defer
-          src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_ID}`}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${NEXT_PUBLIC_GA_ID}', {
-            page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
         <Layout>
           <Component {...pageProps} />
           <Footer />
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+// eslint-disable-next-line @next/next/no-script-in-document
+import Script from "next/script";
 export default class MyDocument extends Document {
   render() {
+    const NEXT_PUBLIC_GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+    const NEXT_PUBLIC_GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
     return (
       <Html lang="cs-cz">
         <Head>
@@ -15,7 +19,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100;400;700&family=Tillana&display=swap"
             rel="stylesheet"
           /> */}
-          <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;700&display=swap" rel="stylesheet"></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;700&display=swap"
+            rel="stylesheet"
+          ></link>
           {/* <meta name="robots" content="follow, index" />
           <meta name="description" content={meta.description} />
           <meta property="og:site_name" content={meta.title} />
@@ -27,8 +34,45 @@ export default class MyDocument extends Document {
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
           <meta name="twitter:image" content={meta.image} /> */}
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <Script
+            strategy="afterInteractive"
+            defer
+            src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GA_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+              });
+              `,
+            }}
+          />
+          {/* Google Tag Manager */}
+          <Script
+            id="gtm-start"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${NEXT_PUBLIC_GTM_ID}');`,
+            }}
+          />
         </Head>
         <body className="text-stone-900 bg-stone-100 font-nunito">
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
         </body>
