2104eab6406dbc5d45fb9eb29d8228e909a559ec
mstanka <stmmarketa@gmail.com>
2025-07-18 14:01:54 +0200

    home update

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import CustomImage from "./CustomImage";
 
 export default function Home() {
   return (
     <>
-      <div className="mt-6">
+      <div className="my-6">
         <h1>Proč na kopec</h1>
         <p>
           Na kopec je blog, který vypráví můj příběh, proč jdu &#34;na
@@ -54,8 +55,35 @@ export default function Home() {
           uvidíte tu spousty mých fotek, a semtam nějakou tu myšlenku, která mě
           potká, jak si tak pluju po proudu života…
         </p>
+
         <p>
-          <i>A možná tu najdete inspiraci pro svoji vlastní cestu…</i>
+          A možná tu najdete inspiraci pro svoji vlastní cestu, pro svůj vlastní
+          kopec, který se chystáte zdolat. Protože každý z nás má svůj kopec,
+          který musí zdolat. Ať už je to cokoliv - ať už je to malý kopeček nebo
+          velká hora, důležité je, že se na něj vydáme a že se nevzdáme, když
+          narazíme na překážky.
+        </p>
+
+        <p>
+          Moje chození po horách mě nakonec přivedlo ke cvičení Taiji. Postupně
+          a pomalu mi začalo pronikat do života a já jsem si uvědomila, že tohle
+          je moje cesta. Cesta, která mi dává smysl a která mě naplňuje. Protože
+          Taiji je pro mě jako chození po horách - je to cesta, která mě vede k
+          sobě samé, k mému vnitřnímu klidu a harmonii.
+        </p>
+
+        <div className="flex justify-center pb-2">
+          <CustomImage
+            src="/images/general/taiji.jpg"
+            alt="Taiji"
+            size="verticalLarge"
+          />
+        </div>
+
+        <p>
+          Takže pokud máte chuť, pojďte se mnou na kopec. Pojďte objevovat krásy
+          hor, přírody a života. Pojďte hledat cestu k sobě samým a k svému
+          vnitřnímu klidu a harmonii.
         </p>
       </div>
 
