1d76aa7199d261f1638084d003031302a5ad38a4
mstanka <stmmarketa@gmail.com>
2025-08-14 20:47:45 +0200

    fix layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 export default function Layout({ children }) {
   return (
     <>
-      <div className="max-w-screen-2xl mx-auto min-h-screen bg-stone-200 dark:bg-gray-800 md:pb-24 px-4 md:px-10 xl:px-20">
+      <div className="max-w-screen-2xl mx-auto min-h-screen pb-24 bg-stone-200 dark:bg-gray-800 md:pb-24 px-4 md:px-10 xl:px-20">
         {children}
       </div>
     </>
